6fa83e23f7bbb3fe8504b3814ffd0e3aef56856e
Maxim Skryabin <dev@mskr.name>
2020-04-05 16:14:45 +0300

    Project created

---
+++ foodcontrol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  foodcontrol
+//
+//  Created by Maxim Skryabin on 05.04.2020.
+//  Copyright © 2020 MSKR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
